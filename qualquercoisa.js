56324d4bdf506702a9d02320a4d2cbdf817e0157
Kauan-Bruno <kauan.souza11@escola.pr.gov.br>
2025-10-31 23:46:43 -0300

    d

--- qualquercoisa.js
+++ qualquercoisa.js
@@ -1,8 +1,23 @@
-function mudarCores() {
-    setInterval(() => {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    }, 100); // muda a cada 0.1 segundo
+let intervaloCores;
+let modoAtivo = false;
+
+function ativarModoMaluco() {
+    const musica = document.getElementById("musica");
+
+    if (!modoAtivo) {
+        modoAtivo = true;
+        musica.play();
+
+        intervaloCores = setInterval(() => {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }, 100);
+    } else {
+        modoAtivo = false;
+        musica.pause();
+        musica.currentTime = 0;
+        clearInterval(intervaloCores);
+    }
 }
